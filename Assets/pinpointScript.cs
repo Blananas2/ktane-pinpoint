66bd46cfb5c65f5cc201b374ae00cece73ec1c42
Blananas2 <blananas2@gmail.com>
2025-01-16 09:32:33 -0500

    arm and hue shift animations

--- Assets/pinpointScript.cs
+++ Assets/pinpointScript.cs
@@ -16,13 +16,16 @@ public class pinpointScript : MonoBehaviour {
     public SpriteRenderer Arm;
     public GameObject DistanceObj;
     public TextMesh Distance;
+    public Material ColorMat;
 
     int[] points = { -1, -1, -1, -1 }; //position in reading order; was going to use a class for this but this is what _Zero, Zero_ does
     int[] pointXs = { -1, -1, -1, -1 };
     int[] pointYs = { -1, -1, -1, -1 };
     float scaleFactor = -1f;
     float[] dists = { -1f, -1f, -1f };
-    float WAITTIME = 2f;
+    float HUESCALE = 0.0005f;
+    int shownPoint = 0;
+    float WAITTIME = 4f;
     float ZIPTIME = 0.5f;
     float[] posLUT = { -0.055f, -0.042777f, -0.030555f, -0.018333f, -0.006111f, 0.006111f, 0.018333f, 0.030555f, 0.042777f, 0.055f };
     bool submissionMode = false;
@@ -40,7 +43,6 @@ public class pinpointScript : MonoBehaviour {
         }
     }
 
-    // Use this for initialization
     void Start () {
         scaleFactor = Rnd.Range(18, 7857) * 0.001f; //scale factors in this range ensure that 1) all the possible hypotenuses have distinct values when truncated to 3 decimals of precision and 2) the maximum a scaled hypotenuse is under 100
         Debug.Log("Scale factor: " + scaleFactor);
@@ -65,7 +67,19 @@ public class pinpointScript : MonoBehaviour {
         }
 
         Debug.Log("Distances: " + dists.Join(", "));
+        UpdateDistanceArm();
+        StartCoroutine(HueShift());
         StartCoroutine(MoveSquare());
+    }
+
+    private IEnumerator HueShift () {
+        float elapsed = Rnd.Range(0f, 1f/HUESCALE);
+        while (true) {
+            ColorMat.color = Color.HSVToRGB(elapsed * HUESCALE, 0.5f, 1f);
+            yield return null;
+            elapsed += Time.deltaTime;
+            if (elapsed * HUESCALE > 1f) { elapsed = 0f; }
+        }
     }
 
     void PositionPress (KMSelectable P) {
@@ -79,24 +93,48 @@ public class pinpointScript : MonoBehaviour {
 
     private IEnumerator MoveSquare () {
         float elapsed = 0f;
-        int loc = 0;
         while (true) {
             if (elapsed < WAITTIME) {
-                Square.transform.localPosition = new Vector3(posLUT[pointXs[loc]], 0.02f, -posLUT[pointYs[loc]]);
+                Arm.gameObject.SetActive(true);
+                DistanceObj.SetActive(true);
+                Color opc = new Vector4(1f, 1f, 1f, lerp(1f, 0f, Math.Abs(elapsed - WAITTIME/2)/(WAITTIME/2)));
+                Arm.color = opc;
+                Distance.color = opc;
             } else {
-                Square.transform.localPosition = new Vector3(lerp(posLUT[pointXs[loc]], posLUT[pointXs[(loc+1)%3]], (elapsed - WAITTIME)*(1/ZIPTIME)), 0.02f, lerp(-posLUT[pointYs[loc]], -posLUT[pointYs[(loc+1)%3]], (elapsed - WAITTIME)*(1/ZIPTIME)));
+                Arm.gameObject.SetActive(false);
+                DistanceObj.SetActive(false);
+                Square.transform.localPosition = new Vector3(lerp(posLUT[pointXs[shownPoint]], posLUT[pointXs[(shownPoint+1)%3]], (elapsed - WAITTIME)*(1/ZIPTIME)), 0.02f, lerp(-posLUT[pointYs[shownPoint]], -posLUT[pointYs[(shownPoint+1)%3]], (elapsed - WAITTIME)*(1/ZIPTIME)));
             }
             yield return null;
             elapsed += Time.deltaTime;
             if (elapsed > (WAITTIME + ZIPTIME)) {
-                elapsed -= (WAITTIME + ZIPTIME);
-                loc = (loc + 1) % 3;
+                shownPoint = (shownPoint + 1) % 3;
+                UpdateDistanceArm();
+                elapsed = 0f;
             }
         }
+    }
+
+    void UpdateDistanceArm() {
+        Square.transform.localPosition = new Vector3(posLUT[pointXs[shownPoint]], 0.02f, -posLUT[pointYs[shownPoint]]);
+        Arm.flipX = Square.transform.localPosition.x > 0f;
+        Arm.flipY = Square.transform.localPosition.z < 0f;
+        DistanceObj.transform.localPosition = new Vector3(Square.transform.localPosition.x > 0f ? -0.386f : 0.386f, 0.15f, Square.transform.localPosition.z < 0f ? 0.85f : -0.725f);
+        Distance.text = trunc(dists[shownPoint]);
     }
 
     float lerp(float a, float b, float t) { //this assumes t is in the range 0-1
         return a*(1f-t) + b*t;
     }
 
+    string trunc(float f) {
+        string s = f.ToString();
+        if (s.IndexOf('.') == -1) {
+            return s + ".000";
+        } else {
+            string[] c = s.Split('.');
+            c[1] = c[1].PadRight(3, '0').Substring(0, 3);
+            return c[0] + "." + c[1];
+        }
+    }
 }
